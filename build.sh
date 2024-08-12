b807808942039218aa4b0ade57fd462e9bf427da
JJ Chen <2905873+jiajie-chen@users.noreply.github.com>
2024-08-12 02:44:19 +0000

    Add `copr_enable` function
    Works around the issue of `dnf copr` not being available.
    
    Signed-off-by: JJ Chen <2905873+jiajie-chen@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -7,6 +7,23 @@ set -xu
 
 readonly RELEASE="$(rpm -E %fedora)"
 
+# For COPR enablement, pass in `user` and `project`
+function copr_enable {
+  if [[ "$#" -ne 2 ]]; then
+    printf '%s expected 2 arguments, got %i' "$0" "$#"
+    return 1
+  fi
+  local copr_user="$1"
+  local copr_project="$2"
+  local copr_dest="/usr/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:$copr_user:$copr_project.repo"
+  local copr_url="https://copr.fedorainfracloud.org/coprs/$copr_user/$copr_project/repo/fedora-$RELEASE/$copr_user-$copr_project-fedora-$RELEASE.repo"
+  if wget --secure-protocol=TLSv1_3 -O "$copr_dest" "$copr_url"; then
+    # TODO: maybe some checksumming, GPG verification, etc.
+    return 0
+  fi
+  return 1
+}
+
 ## Installations
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -16,7 +33,7 @@ readonly RELEASE="$(rpm -E %fedora)"
 # Should be able to use `dnf` for builds:
 #   https://github.com/coreos/rpm-ostree/issues/718#issuecomment-2125711817
 
-dnf copr enable swayfx/swayfx \
+copr_enable swayfx swayfx \
 && dnf install --setopt=install_weak_deps=false -y swayfx \
 && dnf install -y sway-systemd swayidle qt5-qtwayland qt6-qtwayland \
 || exit 1
