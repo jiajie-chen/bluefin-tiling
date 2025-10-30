c33502499e1348a3140e3975d8987ae707ea8c25
Jiajie Chen <2905873+jiajie-chen@users.noreply.github.com>
2025-10-29 23:33:25 -0400

    Fix `/etc` vs. `/usr/etc` changes in bootc

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## NOTE: For /etc vs. /usr/etc, see: https://github.com/ublue-os/bluefin/pull/441#issuecomment-1694785648
+## NOTE: For /etc vs. /usr/etc, see: https://bootc-dev.github.io/bootc/filesystem.html#usretc
 
 ## NOTE: change `set` flags to accommodate edge cases
 set -ouex pipefail
@@ -90,20 +90,12 @@ dnf5 install --setopt=install_weak_deps=false -y waybar
 ### Configurations
 
 ## Overwrite the default Sway configs for Bluefin DX
-## Also moving to /usr/etc/ since it will be overlaid onto /etc/
-mkdir -p /usr/etc/sway/
-mv -n /etc/sway/* /usr/etc/sway/
 ## FIXME(2025-10-28): need to update checksum
-## overwrite_with_checksum /tmp/configs/sway/config "$(cat /tmp/configs/sway/config.orig.sha1)" /usr/etc/sway/config
-
-## Move the default Waybar configs
-mkdir -p /usr/etc/xdg/waybar/
-mv -n /etc/xdg/waybar/* /usr/etc/xdg/waybar/
+## overwrite_with_checksum /tmp/configs/sway/config "$(cat /tmp/configs/sway/config.orig.sha1)" /etc/sway/config
 
 ## Add default Onagre configs
 ## FIXME(2025-10-28): Onagre builds not workin ATM
-# mkdir -p /usr/etc/xdg/onagre/
-# install -Dm0644 /tmp/configs/onagre/theme.scss /usr/etc/xdg/onagre/
+# install -Dm0644 /tmp/configs/onagre/theme.scss /etc/xdg/onagre/
 
 ### Finishing
 
