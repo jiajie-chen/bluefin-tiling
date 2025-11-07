b2706e3c84d92c0cd70ef88a25a2ce459d4d6d55
Jiajie Chen <2905873+jiajie-chen@users.noreply.github.com>
2025-11-06 19:26:04 -0500

    Fix up installation of `cosmic-session`

--- build_files/build.sh
+++ build_files/build.sh
@@ -53,7 +53,8 @@ _dnf5_helper install --setopt=install_weak_deps=false waybar
 
 ## COSMIC (Testing)
 ## See: https://packages.fedoraproject.org/pkgs/cosmic-session/cosmic-session/
-_dnf5_helper install cosmic-session --exclude='cosmic-term,cosmic-greeter'
+## TODO(2025-11-06): no way to exclude required deps, without manual RPM download and install
+_dnf5_helper install cosmic-session # --exclude='cosmic-term,cosmic-greeter'
 
 ## Dotnet (for `git-credential-manager`)
 ## Assumes local bootstrapping for a user
